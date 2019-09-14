1cedfcbf92697740b1e866f40d5e3c8d93b36da8
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-09-14 12:16:09 +0300

    Add issue 23 example

--- tests/Examples.hs
+++ tests/Examples.hs
@@ -2,6 +2,8 @@ module Main (main) where
 
 import Test.HUnit ((@?=))
 
+import qualified System.Random as R
+import qualified System.Random.SplitMix as SM
 import qualified System.Random.SplitMix32 as SM32
 
 main :: IO ()
@@ -13,3 +15,19 @@ main = do
     let (w32, g') = SM32.nextWord32 g
     w32     @?= 1296549791
     show g' @?= "SMGen 1747133669 1604540297"
+
+    issue23
+
+issue23 :: IO ()
+issue23 = do
+    let seed = (10000, 10000)
+    let (txt, g) = populateRandomString (SM.seedSMGen' seed)
+    txt @?= "QH"
+    print g
+
+  where
+    populateRandomString :: SM.SMGen -> (String, SM.SMGen)
+    populateRandomString smGen =
+        let (char, ngen)  = R.randomR ('A', 'Z') smGen
+            (char2, gen2) = R.randomR ('A', 'Z') ngen
+        in ([char, char2], gen2)
