5a83a470c4e03f0f061d9899ed203d2ccd9c5a2d
William Roe <git@wjlr.org.uk>
2025-10-27 16:10:05 +0000

    Guard include of sys/random.h by Unixes that have it
    OpenBSD doesn't have this header, for instance.

--- cbits-unix/init.c
+++ cbits-unix/init.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <unistd.h>
+
+#if defined(__linux__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__ANDROID__) || defined(__APPLE__)
 #include <sys/random.h>
+#endif
 
 uint64_t splitmix_init() {
 	uint64_t result;
